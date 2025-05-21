9be38637633bed16c035512b087b58929f5130e3
apc518 <apc518@ymail.com>
2025-05-21 01:15:52 -0700

    handle bad input better

--- htmlHandlers.js
+++ htmlHandlers.js
@@ -4,15 +4,26 @@ const textFieldOkayColor = "#fff";
 const mtsInput = document.getElementById("mtsInput");
 const mtsErrorMessage = document.getElementById("mtsError");
 
-mtsInput.oninput = () => {
-    const s = mtsInput.value;
+mtsInput.value = "[3,2*4]";
+mtsInput.style.backgroundColor = mtsStringIsValid(mtsInput.value) ? textFieldOkayColor : textFieldErrorColor;
+let mtsInputPreviousContent = mtsInput.value;
 
+mtsInput.oninput = e => {
     // remove invalid characters immediately
-    for (let i = 0; i < s.length; i++){
-        if (!(validMtsCharacters.includes(s[i]))){
-            mtsInput.value = mtsInput.value.replaceAll(s[i], "");
+    if (e.data){
+        let initialCursorPosition = mtsInput.selectionStart;
+        for (let i = 0; i < e.data.length; i++){
+            if (!(validMtsCharacters.includes(e.data[i]))){
+                mtsInput.value = mtsInputPreviousContent;
+                setMtsErrorMessage(`Input \"${e.data}\" contains invalid characters. Valid characters: \"${validMtsCharacters}\"`);
+                mtsInput.selectionStart = initialCursorPosition - e.data.length;
+                mtsInput.selectionEnd = initialCursorPosition - e.data.length;
+                return;
+            }
         }
     }
+
+    mtsInputPreviousContent = mtsInput.value.slice();
     
     mtsInput.inputIsValid = mtsStringIsValid(mtsInput.value);
 
@@ -26,8 +37,6 @@ mtsInput.oninput = () => {
     fullRefresh();
 }
 
-mtsInput.value = "[3,2*4]";
-mtsInput.style.backgroundColor = mtsStringIsValid(mtsInput.value) ? textFieldOkayColor : textFieldErrorColor;
 
 function setMtsErrorMessage(s){
     mtsErrorMessage.textContent = s;

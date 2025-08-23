3c6b1517870d9f8557e7b1e21e37900d7bbec128
apc518 <apc518@ymail.com>
2025-08-23 03:18:24 -0700

    highlight hue control overhaul

--- htmlHandlers.js
+++ htmlHandlers.js
@@ -118,25 +118,25 @@ function setTempoInputFromCurrentPatch(){
 //  NODE NUMBER MODE SETTING   //
 /////////////////////////////////
 
-const nodeNumberModeDropdown = document.getElementById("nodeNumberModeDropdown");
-for (let item of nodeNumberModeOptions){
-    let leavesOption = document.createElement('option');
-    leavesOption.value = item;
-    leavesOption.innerText = item;
-    nodeNumberModeDropdown.appendChild(leavesOption);
-}
-nodeNumberModeDropdown.oninput = () => {
-    setPatchParam("nodeNumberMode", nodeNumberModeDropdown.children[nodeNumberModeDropdown.selectedIndex].value);
-    paint();
-}
+// const nodeNumberModeDropdown = document.getElementById("nodeNumberModeDropdown");
+// for (let item of nodeNumberModeOptions){
+//     let leavesOption = document.createElement('option');
+//     leavesOption.value = item;
+//     leavesOption.innerText = item;
+//     nodeNumberModeDropdown.appendChild(leavesOption);
+// }
+// nodeNumberModeDropdown.oninput = () => {
+//     setPatchParam("nodeNumberMode", nodeNumberModeDropdown.children[nodeNumberModeDropdown.selectedIndex].value);
+//     paint();
+// }
 
 function setNumberModeInputFromCurrentPatch(){
-    for (let i = 0; i < nodeNumberModeDropdown.children.length; i++){
-        if (nodeNumberModeDropdown.children[i].value === currentPatch.nodeNumberMode){
-            nodeNumberModeDropdown.selectedIndex = i;
-            break;
-        }
-    }
+    // for (let i = 0; i < nodeNumberModeDropdown.children.length; i++){
+    //     if (nodeNumberModeDropdown.children[i].value === currentPatch.nodeNumberMode){
+    //         nodeNumberModeDropdown.selectedIndex = i;
+    //         break;
+    //     }
+    // }
 }
 
 
@@ -203,37 +203,40 @@ function setClickSoundSettingsFromCurrentPatch(){
 //  COLOR   //
 //////////////
 
-function rgbArrayToHex(rgbArray){
-    let hex = "#";
-
-    for (let value of rgbArray){
-        hex += Number(value).toString(16).padStart(2, "0");
-    }
-
-    return hex;
-}
-
-function hexToRgbArray(hex){
-    if (hex.length !== 7) throw new Error("hex string must be exactly 7 characters");
-    if (!hex.startsWith("#")) throw new Error("hex string must start with '#'");
-    
-    const r = parseInt(hex.slice(1, 3), 16);
-    const g = parseInt(hex.slice(3, 5), 16);
-    const b = parseInt(hex.slice(5, 7), 16);
-
-    return [r,g,b];
-}
-
 const hueInput = document.getElementById("hueInput");
+const hueInputCursor = document.getElementById("hueInputCursor");
 
 function setColorInputsFromCurrentPatch(){
-    hueInput.value = currentPatch.hue;
-    hueInput.style.accentColor = getPatchHighlightColor();
+    const hueInputWidth = hueInput.getBoundingClientRect().width;
+    hueInputCursor.style.marginLeft = `${Math.round(hueInputWidth * currentPatch.hue / 360)}px`;
 }
 
-hueInput.oninput = () => {
-    setPatchParam("hue", hueInput.value);
-    hueInput.style.accentColor = getPatchHighlightColor();
+let changingHue = false;
+
+hueInput.onmousedown = e => {
+    changingHue = true;
+}
+
+window.onmouseup = () => {
+    changingHue = false;
+}
+
+hueInput.onmousemove = e => {
+    if (changingHue){
+        changeHue(e);
+    }
+}
+
+
+hueInput.onclick = e => {
+    changeHue(e);
+}
+
+function changeHue(e){
+    const hueInputX = hueInput.getBoundingClientRect().x;
+    const hueInputWidth = hueInput.getBoundingClientRect().width;
+    hueInputCursor.style.marginLeft = `${e.clientX - hueInputX}px`;
+    setPatchParam("hue", Math.round(360 * (e.clientX - hueInputX) / hueInputWidth));
     if (!isLooping()) {
         paint();
     }
